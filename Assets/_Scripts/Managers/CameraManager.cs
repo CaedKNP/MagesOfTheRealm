aff802244b2348bdcbfa3822e67063f4b63b1eae
Nazarii Shchehliuk <nazar240902@gmail.com>
2023-03-06 23:38:52 +0100

    Player Movement (input package)

---
+++ Assets/_Scripts/Managers/CameraManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    public Vector2 minPos;
+    public Vector2 maxPos;
+
+    private void FixedUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
+            targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        }
+    }
+}
