eb9c92d216cf2f068ad91d44e0e47e0dba561547
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-03-14 20:24:07 +0100

    Creating fire ball spell and staff
    -added fireballSpell - problem with collision - so destroy everything 
    -wizzardStaff fix
    -player pixel art filter fix

---
+++ Assets/_Scripts/Spells/CastingSpell.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastingSpell : MonoBehaviour
+{
+    [SerializeField] private GameObject pfSpell;
+
+    private void Update()
+    {
+        
+    }
+
+    
+}
