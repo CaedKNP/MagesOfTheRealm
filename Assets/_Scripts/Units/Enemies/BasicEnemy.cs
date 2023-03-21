8ff5545af393ca9c3644e0e53795654d2d2f0eeb
Caed <32883053+CaedKNP@users.noreply.github.com>
2023-03-21 13:41:32 +0100

    Change enemy collision

--- Assets/_Scripts/Units/Enemies/BasicEnemy.cs
+++ Assets/_Scripts/Units/Enemies/BasicEnemy.cs
@@ -11,6 +11,10 @@ public class BasicEnemy : MonoBehaviour
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
 
+    List<RaycastHit2D> castCollisions = new();
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,6 +32,31 @@ public class BasicEnemy : MonoBehaviour
     {
         Vector2 direction = _targetPosition - rb.position;
         direction.Normalize();
-        rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * direction);
+        TryMove(direction);
     }
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            // Check for potential collisions
+            int count = rb.Cast(
+                direction, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
+                movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
+                castCollisions, // List of collisions to store the found collisions into after the Cast is finished
+                moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
+
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + moveSpeed * Time.fixedDeltaTime * direction);
+                return true;
+            }
+
+            return false;
+        }
+
+        // Can't move if there's no direction to move in
+        return false;
+    }
+
 }
