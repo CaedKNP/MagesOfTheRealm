c1918d1b7da753d735c79d8ba1988ed6700faa42
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-03-28 12:45:34 +0200

    cast spells from spellCastPanel

--- Assets/_Scripts/Spells/SpellCastPanel.cs
+++ Assets/_Scripts/Spells/SpellCastPanel.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellCastPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] spells; // tablica czarów
+    private StaffRotation spellRotator; // referencja do rotatora
+    private GameObject player; // referencja do obiektu gracza
+
     void Start()
     {
-        
+        FindSpellRotator();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FindSpellRotator()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        spellRotator = player.GetComponentInChildren<StaffRotation>();
+        if (spellRotator == null)
+        {
+            Debug.LogWarning("spellRotator is null!");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            CastSpell(0); // rzucanie pierwszego czaru
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            CastSpell(1); // rzucanie drugiego czaru
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            CastSpell(2); // rzucanie trzeciego czaru
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            CastSpell(3); // rzucanie czwartego czaru
+        }
+    }
+
+    void CastSpell(int index)
+    {
+        Debug.Log("Casting spell " + index);
+        if (spellRotator != null)
+        {
+            Instantiate(spells[index], spellRotator.spellTransform.position, spellRotator.transform.rotation);
+        }
+        else
+        {
+            Debug.LogWarning("SpellRotator is not assigned!");
+            FindSpellRotator();
+            //Instantiate(spells[index], spellRotator.spellTransform.position, spellRotator.transform.rotation);
+        }
     }
 }
