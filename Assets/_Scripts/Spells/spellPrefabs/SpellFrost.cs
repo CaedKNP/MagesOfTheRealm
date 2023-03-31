a692522c7caffd66ec95c7c3d436f5dd948a1b43
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-03-31 20:43:33 +0200

    added a Trail Renderer to the spells for tests

--- Assets/_Scripts/Spells/spellPrefabs/SpellFrost.cs
+++ Assets/_Scripts/Spells/spellPrefabs/SpellFrost.cs
@@ -5,11 +5,24 @@ public class SpellFrost : MonoBehaviour
     float speed = 2f; // prędkość ruchu prefaba
 
     private Rigidbody2D rb;
+    private TrailRenderer trailRenderer;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>(); // pobieramy Rigidbody2D komponent z prefabu
         rb.velocity = transform.right * speed; // ustawiamy prędkość w kierunku "przodu" prefabu
+
+        //// dodajemy komponent Trail Renderer
+        //trailRenderer = gameObject.AddComponent<TrailRenderer>();
+
+        //// ustawiamy kolor linii na niebieski
+        //trailRenderer.startColor = Color.blue;
+        //trailRenderer.endColor = Color.blue;
+
+        //// ustawiamy grubość linii na 3
+        //trailRenderer.startWidth = 3f;
+        //trailRenderer.endWidth = 3f;
+
         Destroy(gameObject, 2);
     }
 
@@ -19,6 +32,5 @@ public class SpellFrost : MonoBehaviour
         {
             Destroy(gameObject);
         }
-        //Destroy(collision.gameObject);
     }
 }
