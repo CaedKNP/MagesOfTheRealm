864d897dd27070945a653d57ba6666eea7b7cef3
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-04-26 01:01:07 +0200

    Working on SpellDash

---
+++ Assets/Prefabs/Spells/SpellDash/SpellDash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : SpellBase
+{
+    public GameObject player;
+
+    protected void Awake()
+    {
+        SetSpeedDestroyTime(1f, 3f); // Nowe wartości dla speed i destroyTime
+        base.MyAwake();
+    }
+
+    void Update()
+    {
+        Vector3 direction = transform.position - player.transform.position;
+        player.transform.position += direction.normalized * Time.deltaTime * 5f;
+    }
+}
