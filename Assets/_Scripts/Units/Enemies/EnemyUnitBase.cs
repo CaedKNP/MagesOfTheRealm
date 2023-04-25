5f31e5d8d2ee99489f3e2844f20c8a42aba03f96
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-04-25 22:58:36 +0200

    Push orgin to SpellSystem and andded some bugs

---
+++ Assets/_Scripts/Units/Enemies/EnemyUnitBase.cs
@@ -0,0 +1,35 @@
+using Assets._Scripts.Utilities;
+using UnityEngine;
+
+public class EnemyUnitBase : UnitBase
+{
+    public override void SetStats(Stats stats)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void TakeDamage(int dmg)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool TryMove(Vector2 direction)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void LockMovement()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UnlockMovement()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+}
