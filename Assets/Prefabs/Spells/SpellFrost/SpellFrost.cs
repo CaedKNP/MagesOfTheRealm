fca7687a99f687a30f4b31ce5e7ff20bb7a029c2
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-05-09 19:21:35 +0200

    SpellDarkMeteor

--- Assets/Prefabs/Spells/SpellFrost/SpellFrost.cs
+++ Assets/Prefabs/Spells/SpellFrost/SpellFrost.cs
@@ -4,7 +4,7 @@ public class SpellFrost : SpellBase
 {
     protected void Awake()
     {
-        SetSpeedDestroyTime(2f, 1f); // Nowe wartości dla speed i destroyTime
+        SetSpeedDestroyTime(1.5f, 2f); // Nowe wartości dla speed i destroyTime
         base.MyAwake();
     }
 }
