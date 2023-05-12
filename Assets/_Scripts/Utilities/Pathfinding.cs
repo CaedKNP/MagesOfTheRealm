7ee5c046eb9aee76b44719f93391e5345483033d
Caed <32883053+CaedKNP@users.noreply.github.com>
2023-05-12 19:01:04 +0200

    Added straight line for pathfinding

--- Assets/_Scripts/Utilities/Pathfinding.cs
+++ Assets/_Scripts/Utilities/Pathfinding.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -7,12 +8,14 @@ using UnityEngine.UIElements;
 public class Pathfinding : MonoBehaviour
 {
     float scale = 1.6f;
-    float dotSize = 0.02f;
+    float dotSize = 0.2f;
     Vector2 offset = new Vector2(0f, 0f);
     Vector2[,] mapVector;
+    List<Vector2Int> path;
+    List<Vector2> pathVectors;
 
     // Update is called once per frame
-    
+
 
     private void GenerateVectorMap()
     {
@@ -33,6 +36,7 @@ public class Pathfinding : MonoBehaviour
     }
     private void Start()
     {
+        path = new List<Vector2Int>();
         GenerateVectorMap();
     }
 
@@ -40,19 +44,86 @@ public class Pathfinding : MonoBehaviour
     {
         if (Application.IsPlaying(this))
         {
+            GeneratePath(new Vector2(0, 0), (Vector2)GameManager.Player.transform.position);
+            //Gizmos.color = Color.red;
+            //Vector2Int pos = GetStandingTile(new Vector2(35, 18));
+            //Gizmos.DrawSphere(mapVector[pos.x, pos.y], dotSize);
             Gizmos.color = Color.blue;
-            //Vector2Int pos = GetStandingTile(GameManager.Player.transform.position);
-            //Gizmos.DrawSphere(mapVector[pos.x, pos.x], 0.5f);
-            foreach (Vector2 v in mapVector)
-                Gizmos.DrawSphere(v, 0.5f);
+
+            foreach (Vector2Int v in path)
+                Gizmos.DrawSphere(mapVector[v.x, v.y], dotSize);
         }
     }
-    public void GeneratePath(Vector2 origin, Vector2 destination)
+    public void GeneratePath(Vector2 _origin, Vector2 _destination)
     {
-        Vector2Int originMap = GetStandingTile(origin);
-        Vector2Int destMap = GetStandingTile(destination);
+        path.Clear();
+        Vector2Int origin = GetStandingTile(_origin);
+        Vector2Int dest = GetStandingTile(_destination);
+        Vector2Int point = origin;
+        float distance = Vector2Int.Distance(point, dest);
+        path.Add(point);
+        int i = 0;
+        while (dest.x != point.x || dest.y != point.y)
+        {
+            float tempDist = Vector2Int.Distance(new Vector2Int(point.x + 1, point.y), dest);
+            Vector2Int tempPoint = Vector2Int.zero;
+            if (tempDist < distance) // up
+            {
+                tempPoint = new Vector2Int(point.x + 1, point.y);
+                distance = tempDist;
+            }
+            tempDist = Vector2Int.Distance(new Vector2Int(point.x - 1, point.y), dest);
+            if (tempDist < distance) // down
+            {
+                tempPoint = new Vector2Int(point.x - 1, point.y);
+                distance = tempDist;
 
+            }
+            tempDist = Vector2Int.Distance(new Vector2Int(point.x + 1, point.y + 1), dest);
+            if (tempDist < distance) //up right
+            {
+                tempPoint = new Vector2Int(point.x + 1, point.y + 1);
+                distance = tempDist;
 
+            }
+            tempDist = Vector2Int.Distance(new Vector2Int(point.x - 1, point.y + 1), dest);
+            if (tempDist < distance) //down right
+            {
+                tempPoint = new Vector2Int(point.x - 1, point.y + 1);
+                distance = tempDist;
+
+            }
+            tempDist = Vector2Int.Distance(new Vector2Int(point.x - 1, point.y - 1), dest);
+            if (tempDist < distance) //down left
+            {
+                tempPoint = new Vector2Int(point.x - 1, point.y - 1);
+                distance = tempDist;
+
+            }
+            tempDist = Vector2Int.Distance(new Vector2Int(point.x + 1, point.y - 1), dest);
+            if (tempDist < distance) //up left
+            {
+                tempPoint = new Vector2Int(point.x + 1, point.y - 1);
+                distance = tempDist;
+
+            }
+            tempDist = Vector2Int.Distance(new Vector2Int(point.x, point.y - 1), dest);
+            if (tempDist < distance) //left
+            {
+                tempPoint = new Vector2Int(point.x, point.y - 1);
+                distance = tempDist;
+
+            }
+            tempDist = Vector2Int.Distance(new Vector2Int(point.x, point.y + 1), dest);
+            if (tempDist < distance) //right
+            {
+                tempPoint = new Vector2Int(point.x, point.y + 1);
+                distance = tempDist;
+            }
+            point = tempPoint;
+            path.Add(point);
+            i++;
+        }
     }
 
     public Vector2Int GetStandingTile(Vector2 target)
