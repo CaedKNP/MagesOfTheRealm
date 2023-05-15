79a8d74c425adf6f488f72da4e1c9b41f8038a1e
KacperCodeCore <94679062+KacperCodeCore@users.noreply.github.com>
2023-05-16 00:19:26 +0200

    SpellAcidBall

---
+++ Assets/Prefabs/Spells/AcidBall/SpellAcidBall.cs
@@ -0,0 +1,29 @@
+﻿using Assets._Scripts.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellAcidBall : SpellBase
+{
+    protected void Awake()
+    {
+        SetSpeedDestroyTime(7f, 3f); // Nowe wartości dla speed i destroyTime
+        base.MyAwake();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        var conditions = new List<ConditionBase>
+        {
+            new ConditionBase() { Conditions = Conditions.Poison, AffectOnTick = 3f, AffectTime = 3f }
+        };
+
+        if (collision.gameObject.TryGetComponent<UnitBase>(out UnitBase unit))
+        {
+            unit.TakeDamage(3, conditions);
+
+            if (!BeforeDelete())
+                Destroy(gameObject);
+        }
+    }
+}
--- Assets/Prefabs/Spells/Thunder/Part3/Spell3Thunder.cs
+++ Assets/Prefabs/Spells/Thunder/Part3/Spell3Thunder.cs
@@ -1,8 +1,9 @@
+using Assets._Scripts.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spell3Thunder : MonoBehaviour
 {
-
+    ///!!! logika atakowania
 }
