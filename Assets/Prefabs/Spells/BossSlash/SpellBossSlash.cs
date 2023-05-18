c7ceabaee980537edef70b622e2ccd9ff4248a24
MoosieySroosiey <kl1timch@gmail.com>
2023-05-18 08:36:26 +0200

    Fixes+Tweaks+Began BulkSpell-Reskins

---
+++ Assets/Prefabs/Spells/BossSlash/SpellBossSlash.cs
@@ -0,0 +1,44 @@
+using Assets._Scripts.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellBossSlash : SpellBase
+{
+    public Animator animator;
+    protected void Awake()
+    {
+        Animation();
+    }
+
+    void Animation()
+    {
+        animator.speed = animator.speed * 2.5f;
+        animator.enabled = true; // Enable the Animator
+        StartCoroutine(WaitForAnimationToEnd());
+        ExplosiveDamage();
+    }
+
+    IEnumerator WaitForAnimationToEnd()
+    {
+        // Wait until the animation finishes playing
+        while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        {
+            yield return null;
+        }
+
+        // Animation has finished playing, destroy the object
+        Destroy(gameObject);
+    }
+
+    private void ExplosiveDamage()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector2(gameObject.transform.position.x, gameObject.transform.position.y), 4.5f);
+
+        foreach (var collider in hitColliders)
+        {
+            if (collider.TryGetComponent(out UnitBase unit))
+                unit.TakeDamage(2, new List<ConditionBase>());
+        }
+    }
+}
